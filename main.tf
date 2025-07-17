5a2822cdf116323f057fbef69cadbe4495551916
praveenreddy-tech <praveenreddyazure1@gmail.com>
2025-07-17 17:53:15 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,24 +2,7 @@ provider "azurerm" {
   features {}
 }
 
-# Resource Group
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-demo-from-terraform"
+resource "azurerm_resource_group" "example" {
+  name     = "case1-resource-group"
   location = "East US"
-}
-
-# Storage Account
-resource "azurerm_storage_account" "storage" {
-  name                     = "demotfstorage123456"       # must be globally unique
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-# Blob Container
-resource "azurerm_storage_container" "container" {
-  name                  = "mycontainer"
-  storage_account_name  = azurerm_storage_account.storage.name
-  container_access_type = "private"
 }
