a7808795664aac100c259b99055a778e5e3be89b
praveenreddy-tech <praveenreddyazure1@gmail.com>
2025-07-17 14:59:18 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,25 @@
 provider "azurerm" {
-features {}
+  features {}
 }
- 
+
 # Resource Group
 resource "azurerm_resource_group" "rg" {
-name = "tf-jenkins-rg"
-location = "East US"
+  name     = "rg-demo-from-terraform"
+  location = "East US"
 }
+
 # Storage Account
 resource "azurerm_storage_account" "storage" {
-name = "tfjenkinsstoreeee01"
-resource_group_name = azurerm_resource_group.rg.name
-location = azurerm_resource_group.rg.location
-account_tier = "Standard"
-account_replication_type = "LRS"
+  name                     = "demotfstorage123456"       # must be globally unique
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
- 
+
 # Blob Container
-resource "azurerm_storage_container" "blob" {
-name = "mycontainerrrr"
-storage_account_name = azurerm_storage_account.storage.name
-container_access_type = "private"
+resource "azurerm_storage_container" "container" {
+  name                  = "mycontainer"
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "private"
 }
